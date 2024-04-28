9a4634599671a05b7674bb130c0b7f54d73cf92c
Rishad1404 <rishadislam2019@gmail.com>
2024-04-29 00:03:26 +0600

    myCraft Design added

--- src/MyCraftItem/MyCraftItems.jsx
+++ src/MyCraftItem/MyCraftItems.jsx
@@ -1,7 +1,9 @@
 import { useContext, useEffect, useState } from "react";
 import Navbar from "../Shared/Navbar";
 import { AuthContext } from "../AuthContext/AuthProvider";
-import { Link } from "react-router-dom";
+import { GrUpdate } from "react-icons/gr";
+import { MdDeleteOutline } from "react-icons/md";
+import { CiFilter } from "react-icons/ci";
 
 const MyCraftItems = () => {
     const { user } = useContext(AuthContext) || {}
@@ -17,7 +19,10 @@ const MyCraftItems = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <h1 className="text-6xl font-serif text-center my-10">My Products</h1>
+            <div className="bg-base-200 py-5 my-5 text-center">
+                <h1 className="text-6xl font-serif text-center my-10">My Products</h1>
+                <button className="btn bg-[#0097B2] text-white text-lg "><CiFilter /> Filter</button>
+            </div>
             <div className="container mx-auto">
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
@@ -25,19 +30,22 @@ const MyCraftItems = () => {
                         <div key={item._id} className="rounded-md shadow-md dark:bg-gray-50 dark:text-gray-800">
                             <img src={item.photo} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
                             <div className="flex flex-col justify-between p-6 space-y-8">
-                                <div className="space-y-2">
-                                    <h2 className="text-3xl font-semibold tracking-wide">{item.name}</h2>
-                                    <p className="font-bold text-red-700">{item.subcategory}</p>
-                                    <p className="dark:text-gray-800">{item.description}</p>
+                                <div className="space-y-2 text-center">
+                                    <h2 className="text-3xl font-semibold tracking-wide">{item.itemName}</h2>
+                                    <p className="font-bold text-red-700 text-xl">{item.subcategory}</p>
                                 </div>
                                 <div className="flex justify-around">
                                     <h2 className="text-lg font-bold">Price: {item.price} $</h2>
-                                    <h2 className="font-bold">Rating: {item.rating}</h2>
-                                    <h2 className="font-bold text-green-700">Status: {item.status}</h2>
+                                    <h2 className="font-bold text-lg ">Rating: {item.rating}</h2>
                                 </div>
-                                <Link to={`/crafts/${item._id}`}>
-                                    <button type="button" className="w-full p-3 font-semibold tracking-wide rounded-md dark:bg-[#0097B2] dark:text-gray-50">View Details</button>
-                                </Link>
+                                <div className="flex justify-around">
+                                    <h2 className="text-lg font-bold">Customization: <span className={`font-bold ${item.customization === "Yes" ? "text-green-700" : "text-red-700"}`}>{item.customization}</span></h2>
+                                    <h2 className="font-bold text-lg ">Status: <span className="text-green-700">{item.status}</span></h2>
+                                </div>
+                                <div className="flex gap-5">
+                                    <button type="button" className="w-full p-3 font-semibold tracking-wide rounded-md dark:bg-[#0097B2] dark:text-gray-50 flex items-center gap-5 justify-center"><GrUpdate /> Update</button>
+                                    <button type="button" className="w-full p-3 font-semibold tracking-wide rounded-md dark:bg-[#0097B2] dark:text-gray-50 flex items-center gap-5 justify-center"><MdDeleteOutline /> Delete</button>
+                                </div>
                             </div>
                         </div>
                     ))}
